1769980e45ef34164776ad83fbc017bedbde3b48
Sumaya <sheikhsumaya622@gmail.com>
2026-06-15 17:17:54 +0600

    all query solved

---
+++ query.sql
@@ -0,0 +1,66 @@
+--------------- Query - 01 -----------------
+
+SELECT match_id, fixture, base_ticket_price
+FROM Matches
+WHERE tournament_category = 'Champions League'
+AND match_status = 'Available';
+
+
+
+--------------- Query - 02 -----------------
+SELECT user_id, full_name, email
+FROM Users
+WHERE full_name ILIKE 'Tanvir%'
+   OR full_name ILIKE '%Haque%';
+
+
+--------------- Query - 03 -----------------
+
+SELECT 
+ booking_id,
+ user_id,
+ match_id,
+         COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM Bookings
+WHERE payment_status IS NULL;
+
+
+
+--------------- Query - 04 -----------------
+SELECT 
+    b.booking_id,
+    u.full_name,
+    m.fixture,
+    b.total_cost
+FROM Bookings b
+INNER JOIN Users u ON b.user_id = u.user_id
+INNER JOIN Matches m ON b.match_id = m.match_id;
+
+--------------- Query - 05 -----------------
+
+SELECT 
+    u.user_id,
+    u.full_name,
+    b.booking_id
+FROM Users u
+LEFT JOIN Bookings b ON u.user_id = b.user_id;
+
+--------------- Query - 06 -----------------
+SELECT 
+    booking_id,
+    match_id,
+    total_cost
+FROM Bookings
+WHERE total_cost > (
+    SELECT AVG(total_cost) FROM Bookings
+);
+
+--------------- Query - 07 -----------------
+SELECT 
+    match_id,
+    fixture,
+    base_ticket_price
+FROM Matches
+ORDER BY base_ticket_price DESC
+OFFSET 1
+LIMIT 2;
